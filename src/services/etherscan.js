bee3163ee73c1e5567fe5e9f2fe08d70ee2fe51f
arwin azizi <arwin2002@hotmail.se>
2025-12-14 21:36:47 +0100

    commit

--- src/services/etherscan.js
+++ src/services/etherscan.js
@@ -1,131 +1,117 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const API_KEY = import.meta.env.VITE_ETHERSCAN_API_KEY;
 const BASE_URL = 'https://api.etherscan.io/v2/api';
 
-const etherscan = axios.create({
-    baseURL: BASE_URL,
+// Create request with specific chainId
+const makeRequest = (chainId, params) => {
+  return axios.get(BASE_URL, {
     params: {
-        apikey: API_KEY,
-        chainid: 1
-    }
-});
+      apikey: API_KEY,
+      chainid: chainId,
+      ...params,
+    },
+  });
+};
 
 // Get first ever transaction (for accurate wallet age)
-export const getFirstTransaction = async (address) => {
-    const response = await etherscan.get('', {
-      params: {
-        module: 'account',
-        action: 'txlist',
-        address,
-        startblock: 0,
-        endblock: 99999999,
-        page: 1,
-        offset: 1,
-        sort: 'asc',
-      },
-    });
-    return response.data.result[0] || null;
-}
+export const getFirstTransaction = async (address, chainId = 1) => {
+  const response = await makeRequest(chainId, {
+    module: 'account',
+    action: 'txlist',
+    address,
+    startblock: 0,
+    endblock: 99999999,
+    page: 1,
+    offset: 1,
+    sort: 'asc',
+  });
+  return response.data.result[0] || null;
+};
 
 // Get ETH balance
-export const getBalance = async (address) => {
-    const response = await etherscan.get('', {
-        params: {
-            module: 'account',
-            action: 'balance',
-            address,
-            tag: 'latest'
-        }
-    });
-    return response.data.result;
-}
+export const getBalance = async (address, chainId = 1) => {
+  const response = await makeRequest(chainId, {
+    module: 'account',
+    action: 'balance',
+    address,
+    tag: 'latest',
+  });
+  return response.data.result;
+};
 
 // Get transaction list
-export const getTransactions = async (address) => {
-    const response = await etherscan.get('', {
-      params: {
-        module: 'account',
-        action: 'txlist',
-        address,
-        startblock: 0,
-        endblock: 99999999,
-        page: 1,
-        offset: 10000,
-        sort: 'desc',
-
-      },
-    });
-    console.log('txlist response:', response.data);
-    return response.data.result;
+export const getTransactions = async (address, chainId = 1) => {
+  const response = await makeRequest(chainId, {
+    module: 'account',
+    action: 'txlist',
+    address,
+    startblock: 0,
+    endblock: 99999999,
+    page: 1,
+    offset: 10000,
+    sort: 'desc',
+  });
+  return response.data.result;
 };
 
 // Get ERC-20 token transfers
-export const getTokenTransfers = async (address) => {
-    const response = await etherscan.get('', {
-        params: {
-            module: 'account',
-            action: 'tokentx',
-            address,
-            startblock: 0,
-            endblock: 99999999,
-            page: 1,
-            offset: 10000,
-            sort: 'desc'
-        }
-    });
-    return response.data.result;
+export const getTokenTransfers = async (address, chainId = 1) => {
+  const response = await makeRequest(chainId, {
+    module: 'account',
+    action: 'tokentx',
+    address,
+    startblock: 0,
+    endblock: 99999999,
+    page: 1,
+    offset: 10000,
+    sort: 'desc',
+  });
+  return response.data.result;
 };
 
 // Get NFT transfers
-export const getNFTTransfers = async (address) => {
-    const response = await etherscan.get('', {
-        params: {
-            module: 'account',
-            action: 'tokennfttx',
-            address,
-            startblock: 0,
-            endblock: 99999999,
-            page: 1,
-            offset: 10000,
-            sort: 'desc'
-        }
-    });
-    return response.data.result;
-}
+export const getNFTTransfers = async (address, chainId = 1) => {
+  const response = await makeRequest(chainId, {
+    module: 'account',
+    action: 'tokennfttx',
+    address,
+    startblock: 0,
+    endblock: 99999999,
+    page: 1,
+    offset: 10000,
+    sort: 'desc',
+  });
+  return response.data.result;
+};
 
 // Check if address is contract
-export const isContract = async (address) => {
-    const response = await etherscan.get('', {
-        params: {
-            module: 'proxy',
-            action: 'eth_getCode',
-            address,
-            tag: 'latest'
-        }
-    });
-    console.log('eth_getCode response:', response.data);
-    const code = response.data.result;
-    return code && code !== '0x' && code.length > 2;
-}
+export const isContract = async (address, chainId = 1) => {
+  const response = await makeRequest(chainId, {
+    module: 'proxy',
+    action: 'eth_getCode',
+    address,
+    tag: 'latest',
+  });
+  const code = response.data.result;
+  return code && code !== '0x' && code.length > 2;
+};
 
 // Get ETH price
-export const getEthPrice = async () => {
-    const response = await etherscan.get('', {
-        params: {
-            module: 'stats',
-            action: 'ethprice'
-        }
-    });
-    return response.data.result.ethusd;
+export const getEthPrice = async (chainId = 1) => {
+  const response = await makeRequest(chainId, {
+    module: 'stats',
+    action: 'ethprice',
+  });
+  return response.data.result.ethusd;
 };
 
 export default {
-    getFirstTransaction,
-    getBalance,
-    getTransactions,
-    getTokenTransfers,
-    getNFTTransfers,
-    isContract,
-    getEthPrice
+  getFirstTransaction,
+  getBalance,
+  getTransactions,
+  getTokenTransfers,
+  getNFTTransfers,
+  isContract,
+  getEthPrice,
 };
